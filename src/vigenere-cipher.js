c7a4c8bcb16e8f8b5cdd344a9fb14f710544a7aa
AndrewMik <andrew.mikitchuk@gmail.com>
2023-04-13 15:05:52 +0400

    feat: solve vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,88 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(mode = true){
+      this.mode = mode;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(string, key) {
+    if(!string || !key){
+      throw new Error("Incorrect arguments!");
+    }
+
+    let cypher = "";
+
+    for(let i = 0, j = 0; i < string.length; i++){
+      let currentLetter = string[i];
+  
+      if(this.isUpperCase(currentLetter)){
+        let upperLetter = ((currentLetter.charCodeAt() - 65) + (key[j%key.length].toUpperCase().charCodeAt() - 65)) % 26;
+        cypher += String.fromCharCode(upperLetter+65);
+        j++;
+      } else if(this.isLowerCase(currentLetter)){
+        let lowerLetter = ((currentLetter.charCodeAt() - 97) + (key[j%key.length].toLowerCase().charCodeAt() - 97)) % 26;
+        cypher += String.fromCharCode(lowerLetter+97);
+        j++;
+      } else{
+        cypher += currentLetter;
+      }
+    }
+
+    if(this.mode){
+      return cypher.toUpperCase();
+    } else {
+      return cypher.split("").reverse().join("").toUpperCase();
+    }
   }
+
+  decrypt(string, key) {
+    if(!string || !key){
+      throw new Error("Incorrect arguments!");
+    }
+
+    let decodedString = "";
+
+    for(let i = 0, j = 0; i < string.length; i++){
+      let currentLetter = string[i];
+  
+      if(this.isUpperCase(currentLetter)){
+        let upperLetter = ((currentLetter.charCodeAt() - 65) - (key[j%key.length].toUpperCase().charCodeAt() - 65) + 26) % 26;
+        decodedString += String.fromCharCode(upperLetter+65);
+        j++;
+      } else if(this.isLowerCase(currentLetter)){
+        let lowerLetter = ((currentLetter.charCodeAt() - 97) - (key[j%key.length].toLowerCase().charCodeAt() - 97) + 26) % 26;
+        decodedString += String.fromCharCode(lowerLetter+97);
+        j++;
+      } else{
+        decodedString += currentLetter;
+      }
+    }
+
+    if(this.mode){
+      return decodedString.toUpperCase();
+    } else {
+      return decodedString.split("").reverse().join("").toUpperCase();
+    }
+
+  }
+
+  isUpperCase(letter){
+    letter = letter.charCodeAt();
+    if(letter >= 65 && letter <= 90){
+      return true;
+    } else{
+      return false;
+    }
+  };
+  
+  isLowerCase(letter){
+    letter = letter.charCodeAt();
+    if(letter >= 97 && letter <= 122){
+      return true;
+    } else{
+      return false;
+    }
+  };
 }
 
 module.exports = {
